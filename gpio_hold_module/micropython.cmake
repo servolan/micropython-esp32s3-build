48ed14d19a65358ea652217639b4dcb94ed22bf3
servolan <60650745+servolan@users.noreply.github.com>
2026-08-17 14:30:12 +0200

    Update micropython.cmake

--- gpio_hold_module/micropython.cmake
+++ gpio_hold_module/micropython.cmake
@@ -1,1 +1,12 @@
+add_library(usermod_gpio_hold INTERFACE)
+
+target_sources(usermod_gpio_hold INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/gpio_hold.c
+)
+
+target_include_directories(usermod_gpio_hold INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+target_link_libraries(usermod INTERFACE usermod_gpio_hold)
 
